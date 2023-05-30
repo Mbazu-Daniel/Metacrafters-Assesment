06295425066faba6a116e7098daa26f9959ecf84
Mbazu Daniel <mbazudaniel97@gmail.com>
2023-05-30 07:23:24 +0100

    Solidity: update token burn and mint function to receive two parameters

--- Solidity/Token/Bazu.sol
+++ Solidity/Token/Bazu.sol
@@ -2,20 +2,17 @@
 pragma solidity 0.8.18;
 
 //    REQUIREMENTS
-// 1. Your contract will have public variables that store the details about your 
-// coin (Token Name, Token Abbrv., Total Supply)
+// 1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
 // 2. Your contract will have a mapping of addresses to balances (address => uint)
 // 3. You will have a mint function that takes two parameters: an address and a value.
 //    The function then increases the total supply by that number and increases the balance
 //    of the “sender” address by that amount
-// 4. Your contract will have a burn function, which works the opposite of 
-// the mint function, as it will destroy tokens.
-//    It will take an address and value just like the mint functions. 
-// It will then deduct the value from the total supply
+// 4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens.
+//    It will take an address and value just like the mint functions. It will then deduct the value from the total supply
 //    and from the balance of the “sender”.
-// 5. Lastly, your burn function should have conditionals to make sure the balance
-//  of "sender" is greater than or equal
+// 5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal
 //    to the amount that is supposed to be burned.
+
 
 contract BazuToken {
     // public variables here
@@ -27,16 +24,16 @@ contract BazuToken {
     mapping(address => uint256) public balances;
 
     // mint function
-    function mint(uint256 _value) public {
+    function mint(address _address, uint256 _value) public {
         totaSupply += _value;
-        balances[msg.sender] += _value;
+        balances[_address] += _value;
     }
 
     // burn function
-    function burn(uint256 _value) public {
-        if (balances[msg.sender] >= _value) {
+    function burn(address _address, uint256 _value) public {
+        if (balances[_address] >= _value) {
             totaSupply -= _value;
-            balances[msg.sender] -= _value;
+            balances[_address] -= _value;
         }
     }
 }
